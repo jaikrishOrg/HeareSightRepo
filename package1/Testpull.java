c4cb086ce72a78874d57c7d1760c897428d94130
Jayakumar <jayakumar.k@devopslabs.tech>
2022-02-16 08:48:21 +0530

    Test pull

---
+++ package1/Testpull.java
@@ -0,0 +1,11 @@
+public class Ques {
+    public static void main(String[] arg) {
+        int i = 0, j = 1, k, m, count = 10;
+        for (m = 2; m < count; m++){
+            k = i + j;
+            System.out.println(k);
+            i = j;
+            j = k;
+        }
+    }
+}
